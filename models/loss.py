8a0773c03b176c0973b55d6660b7ac4fd9522d72
Alejandra Duran <ad2002@princeton.edu>
2025-01-30 17:43:55 -0500

    update mse loss:

--- models/loss.py
+++ models/loss.py
@@ -1,7 +1,7 @@
 
 def loss_mse(output, target, mask):
     """
-    Mean squared error loss - simple version used by Valente
+    Mean squared error loss 
     :param output: torch tensor of shape (num_trials, num_timesteps, output_dim)
     :param target: idem
     :param mask: torch tensor of shape (num_trials, num_timesteps, 1)
@@ -9,6 +9,7 @@ def loss_mse(output, target, mask):
     """
     # Compute loss for each (trial, timestep) (average accross output dimensions)
     loss_tensor = (mask * (target - output)).pow(2).mean(dim=-1)
-    # Account for different number of masked values per trial
-    loss_by_trial = loss_tensor.sum(dim=-1) / mask[:, :, 0].sum(dim=-1)
-    return loss_by_trial.mean()
+    # Average over timesteps - account for batches of different size
+    loss_by_batch = loss_tensor.sum(dim=-1) / mask[:, :, 0].sum(dim=-1)
+    # Average over batches
+    return loss_by_batch.mean()
